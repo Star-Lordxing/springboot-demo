5f90fd462a71cdc7711b30c1a3ad56c5eda22b08
xing-think <1065754909@qq.com>
2018-12-17 22:07:09 +0800

    spring demo

--- demo/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/hello")
     public String index() {
-        System.out.println("接收请求11：");
         return "Hello World";
     }
 }
